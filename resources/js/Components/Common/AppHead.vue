f642cb59ce082f2407e44f3a5fcfb1eda2540384
HenestrosaDev <henestrosadev@gmail.com>
2023-12-24 17:09:17 +0100

    Add `AppHead` component

---
+++ resources/js/Components/Common/AppHead.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Head } from "@inertiajs/inertia-vue3";
+
+defineProps({
+	title: String,
+	description: String,
+});
+</script>
+
+<template>
+	<Head>
+		<title>{{ title }}</title>
+		<meta
+			name="description"
+			:content="description"
+			head-key="description"
+		/>
+		<link
+			rel="icon"
+			type="image/svg+xml"
+			href="/favicon.svg"
+		/>
+		<slot />
+	</Head>
+</template>
