fb22a88c1b4d0ae87d7dad5a08980bfad46090f1
HenestrosaDev <henestrosadev@gmail.com>
2023-12-24 03:13:18 +0100

    Custom `tailwind.config.js` file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
+function withOpacity(variableName) {
+	return ({ opacityValue }) => {
+		if (opacityValue !== undefined) {
+			return `rgba(var(${variableName}), ${opacityValue})`;
+		}
+		return `rgb(var(${variableName}))`;
+	};
+}
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [
@@ -6,7 +15,30 @@ export default {
 		"./resources/**/*.vue",
 	],
 	theme: {
-		extend: {},
+		colors: {
+			skin: {
+				primary: withOpacity("--color-primary"),
+				secondary: withOpacity("--color-secondary"),
+				tertiary: withOpacity("--color-tertiary"),
+				text: withOpacity("--color-text"),
+				card: withOpacity("--color-card"),
+				border: withOpacity("--color-border"),
+			},
+		},
+		fontFamily: {
+			helvetica: ["Helvetica", "sans-serif"],
+			poppins: ["Poppins", "sans-serif"],
+			"means-web": ["Means Light Web", "sans-serif"],
+		},
+		extend: {
+			screens: {
+				"max-xs": { max: "479px" },
+				"max-sm": { max: "639px" },
+				"max-md": { max: "767px" },
+				"max-lg": { max: "1023px" },
+				"max-xl": { max: "1199px" },
+			},
+		},
 	},
 	plugins: [],
 };
