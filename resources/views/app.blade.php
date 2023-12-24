503927e202c69d1f14da20112ba65082b6a222d0
HenestrosaDev <henestrosadev@gmail.com>
2023-12-24 17:11:04 +0100

    Add `fonts.css` to `app.blade.php`

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 	<script src="https://cdn.tailwindcss.com"></script>
-	@vite(['resources/js/app.js', 'resources/css/app.css'])
+	@vite(['resources/js/app.js', 'resources/css/fonts.css', 'resources/css/app.css'])
 	@inertiaHead
 </head>
 
