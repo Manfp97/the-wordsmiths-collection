21675c5c39bec46cc2673097bc1444fbe5c8dbec
HenestrosaDev <henestrosadev@gmail.com>
2024-01-02 18:19:46 +0100

    Add global progress indicator

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,9 @@ import { createInertiaApp, Link } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
 createInertiaApp({
+	progress: {
+		color: "#d62",
+	},
 	resolve: (name) =>
 		resolvePageComponent(
 			`./Pages/${name}.vue`,
