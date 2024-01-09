e8161a7274dd907e15448929a2a25c361a6f4282
HenestrosaDev <henestrosadev@gmail.com>
2024-01-09 13:45:11 +0100

    Add `IndexBookCard` component (`book` properties might change because I don't have defined the `Book` model yet)

---
+++ resources/js/Components/Index/IndexBookCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+	book: {
+		type: String,
+		required: true,
+	},
+});
+</script>
+
+<template>
+	<div
+		class="my-1 select-none rounded hover:ring-4 hover:ring-skin-border active:ring-skin-secondary"
+		:title="book.title"
+	>
+		<Link
+			:href="`/book/${book.slug}`"
+			class="bg-card block overflow-hidden rounded"
+		>
+			<img
+				:src="book.imageUrl"
+				draggable="false"
+				class="lazy-loaded block h-full w-full object-cover"
+				:alt="book.title"
+			/>
+		</Link>
+	</div>
+</template>
+
+<style scoped>
+.lazy-loaded {
+	@apply opacity-100 transition-opacity duration-[.4s];
+}
+</style>
