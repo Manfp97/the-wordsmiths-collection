e47b59ff6116ea3dc23574d474b0d0b3e4e46680
HenestrosaDev <henestrosadev@gmail.com>
2024-01-25 13:46:23 +0100

    Add `BaseLayout` including the `Alert` component for all website pages

---
+++ resources/js/Layouts/BaseLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Alert from "@/Components/Common/Alert.vue";
+</script>
+
+<template>
+	<div>
+		<slot />
+		<Alert />
+	</div>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,22 @@
 import { createApp, h } from "vue";
 import { createInertiaApp, Link } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import BaseLayout from "@/Layouts/BaseLayout.vue";
 
 createInertiaApp({
 	progress: {
 		color: "#d62",
 	},
-	resolve: (name) =>
-		resolvePageComponent(
+	resolve: (name) => {
+		const page = resolvePageComponent(
 			`./Pages/${name}.vue`,
 			import.meta.glob("./Pages/**/*.vue")
-		),
+		);
+		page.then((module) => {
+			module.default.layout = module.default.layout || BaseLayout;
+		});
+		return page;
+	},
 	setup({ el, App, props, plugin }) {
 		createApp({ render: () => h(App, props) })
 			.use(plugin)
