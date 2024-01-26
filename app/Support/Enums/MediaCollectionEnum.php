c80199ea27be00213c676b4decedcca01af76de1
HenestrosaDev <henestrosadev@gmail.com>
2024-01-26 23:08:27 +0100

    Add `MediaCollectionEnum` and `MediaConversionEnum`

---
+++ app/Support/Enums/MediaCollectionEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Support\Enums;
+
+class MediaCollectionEnum
+{
+	const BOOKS = 'books';
+	const BOOK_COVERS = 'book-covers';
+}
---
+++ app/Support/Enums/MediaConversionEnum.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Support\Enums;
+
+class MediaConversionEnum
+{
+	const WEBP = 'webp';
+}
