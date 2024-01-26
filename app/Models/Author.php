81b49091c1fcaec6572752358af7a70e0c0e5432
HenestrosaDev <henestrosadev@gmail.com>
2024-01-26 23:04:37 +0100

    Add `sluggable()` to `Author` and `Category`

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,16 +6,43 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Author extends Model implements HasMedia
 {
 	use HasFactory;
 	use InteractsWithMedia;
+	use Sluggable;
 
 	protected $fillable = [
 		'first_name',
 		'last_name',
 	];
+
+	/**
+	 * Return the sluggable configuration array for this model.
+	 * Part of Cviebrock\EloquentSluggable Composer package.
+	 *
+	 * @return array
+	 */
+	public function sluggable(): array
+	{
+		return [
+			'slug' => [
+				'source' => ['first_name', 'last_name'],
+				'onUpdate' => true,
+			]
+		];
+	}
+
+	/**
+	 * Get the route key for the model.
+	 * https://laravel.com/docs/10.x/routing#customizing-the-default-key-name
+	 */
+	public function getRouteKeyName(): string
+	{
+		return 'slug';
+	}
 
 	public function books()
 	{
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,12 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Category extends Model
 {
 	use HasFactory;
+	use Sluggable;
 
 	protected $fillable = ['name'];
+
+	/**
+	 * Return the sluggable configuration array for this model.
+	 * Part of Cviebrock\EloquentSluggable Composer package.
+	 *
+	 * @return array
+	 */
+	public function sluggable(): array
+	{
+		return [
+			'slug' => [
+				'source' => 'name',
+				'onUpdate' => true,
+			]
+		];
+	}
+
+	/**
+	 * Get the route key for the model.
+	 * https://laravel.com/docs/10.x/routing#customizing-the-default-key-name
+	 */
+	public function getRouteKeyName(): string
+	{
+		return 'slug';
+	}
 
 	public function books()
 	{
