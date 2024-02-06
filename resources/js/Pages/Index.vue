2fc5b80c7dcad8f4957acfb559e42a76fe3c3ece
HenestrosaDev <henestrosadev@gmail.com>
2024-02-06 10:00:31 +0100

    Change the `Index` to show the `categories` passed as a prop

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -5,22 +5,23 @@ import Footer from "@/Components/Common/Footer.vue";
 import IndexBookCard from "@/Components/Index/IndexBookCard.vue";
 import IndexBooksRow from "@/Components/Index/IndexBooksRow.vue";
 
-const book = {
-	title: "Las Ocho Montañas",
-	slug: "las-ocho-montañas",
-	imageUrl: "/images/index/placeholder.webp",
-};
+defineProps({
+	categories: {
+		type: Array,
+		required: true,
+	},
+});
 </script>
 
 <template>
 	<AppHead />
 
-	<div class="min-h-screen bg-skin-primary-dark">
+	<div class="min-h-screen bg-skin-primary">
 		<div
 			class="hero relative h-[25vh] min-h-[15rem] w-full bg-cover bg-center py-3 text-skin-primary"
 		>
 			<Nav
-				class="lg:px-12"
+				:nav-class="'lg:px-12'"
 				should-invert-icon-color
 				:should-wrap-in-container="false"
 			/>
@@ -37,38 +38,25 @@ const book = {
 			</div>
 		</div>
 
-		<div
-			class="space-y-12 py-10 text-skin-primary sm:space-y-10 lg:space-y-14 lg:py-16"
-		>
-			<IndexBooksRow section-title="Top rated">
+		<div class="space-y-12 py-10 sm:space-y-10 lg:space-y-14 lg:py-16">
+			<IndexBooksRow
+				v-for="(category, indexCategory) in categories"
+				:key="indexCategory"
+				:section-title="category.name"
+			>
 				<swiper-slide
-					v-for="index in Array.from({ length: 20 }, (x, i) => i)"
-					:key="index"
-				>
-					<IndexBookCard :book="book" />
-				</swiper-slide>
-			</IndexBooksRow>
-
-			<IndexBooksRow section-title="Dystopias">
-				<swiper-slide
-					v-for="index in Array.from({ length: 4 }, (x, i) => i)"
-					:key="index"
-				>
-					<IndexBookCard :book="book" />
-				</swiper-slide>
-			</IndexBooksRow>
-
-			<IndexBooksRow section-title="Cult classic">
-				<swiper-slide
-					v-for="index in Array.from({ length: 10 }, (x, i) => i)"
-					:key="index"
+					v-for="(book, indexBook) in category.books.concat(
+						category.books.concat(category.books.concat())
+					)"
+					:key="indexBook"
+					class="my-1 h-auto"
 				>
 					<IndexBookCard :book="book" />
 				</swiper-slide>
 			</IndexBooksRow>
 		</div>
 
-		<Footer class="border-t-4 border-skin-border !bg-transparent" />
+		<Footer class="border-t-4" />
 	</div>
 </template>
 
