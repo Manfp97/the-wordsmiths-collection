3c38be8fb3f7087cf813f4786adf2a853a0fa292
HenestrosaDev <henestrosadev@gmail.com>
2024-02-07 17:56:30 +0100

    Set up `vite-svg-loader` package

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
 	plugins: [
@@ -19,5 +20,6 @@ export default defineConfig({
 				},
 			},
 		}),
+		svgLoader(),
 	],
 });
