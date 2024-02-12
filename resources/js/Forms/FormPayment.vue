49a8c5ccf4666cc78b1143430fe6426acf2fe42f
HenestrosaDev <henestrosadev@gmail.com>
2024-02-12 19:31:55 +0100

    Extract the form from the `Payment` page into a separate `FormPayment`

---
+++ resources/js/Forms/FormPayment.vue
@@ -0,0 +1,191 @@
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+import IconRightArrow from "@icons/right-arrow.svg?component";
+import FloatingLabel from "@/Components/Forms/FloatingLabel.vue";
+
+defineProps({
+	subscriptionPlanName: {
+		type: String,
+		required: false,
+		default: null,
+	},
+	subscriptionPrice: {
+		type: String,
+		required: false,
+		default: null,
+	},
+});
+
+const form = useForm({
+	cardholderName: null,
+	cvv: null,
+	cardNumber: null,
+	expiryMonth: null,
+	expiryYear: null,
+});
+
+const submitForm = () => {
+	form.post("/register/payment", {
+		onSuccess: () => {
+			form.reset();
+			form.clearErrors();
+		},
+	});
+};
+</script>
+
+<template>
+	<form
+		class="space-y-6"
+		@submit.prevent="submitForm"
+	>
+		<FloatingLabel
+			v-model:value="form.cardholderName"
+			input-id="cardholder-name"
+			label-text="Name on card"
+			input-type="text"
+			input-autocomplete="cc-name"
+			:error-message="form.errors.cardholderName"
+		/>
+
+		<div>
+			<span
+				class="mb-2 flex flex-row space-x-2"
+				aria-label="We accept Visa, Mastercard and American Express."
+			>
+				<img
+					v-for="(paymentMethod, index) in ['VISA', 'MASTERCARD', 'AMEX']"
+					:key="index"
+					:src="`/images/register/payment/${paymentMethod}.png`"
+					:srcset="`/images/register/payment/${paymentMethod}@2x.png 2x`"
+					:alt="paymentMethod"
+					class="h-6"
+				/>
+			</span>
+
+			<FloatingLabel
+				v-model:value="form.cardNumber"
+				input-id="card-number"
+				label-text="Card number"
+				input-type="number"
+				input-autocomplete="cc-number"
+				input-mode="numeric"
+				:input-max-length="24"
+				:error-message="form.errors.cardNumber"
+				@input="cardNumber = cardNumber.slice(0, 24)"
+			/>
+		</div>
+
+		<div
+			class="sm:flex sm:flex-row sm:items-center sm:justify-between sm:gap-6 max-sm:space-y-6"
+		>
+			<div
+				class="flex flex-col rounded-lg border border-skin-border bg-skin-white px-4 py-1 sm:w-2/5"
+			>
+				<label class="mt-1 text-xs text-skin-text-muted">Expiration date</label>
+				<div class="flex w-full justify-start space-x-2">
+					<input
+						id="expiryMonth"
+						v-model="form.expiryMonth"
+						type="number"
+						inputmode="numeric"
+						max="12"
+						placeholder="MM"
+						class="w-9 rounded-lg border-none p-0 focus:ring-0"
+						autocomplete="cc-exp-month"
+						@input="
+							(event) => (form.expiryMonth = event.target.value.slice(0, 2))
+						"
+					/>
+					/
+					<input
+						id="expiryYear"
+						v-model="form.expiryYear"
+						type="number"
+						inputmode="numeric"
+						min="24"
+						max="49"
+						placeholder="YY"
+						class="w-9 rounded-lg border-none p-0 focus:ring-0"
+						autocomplete="cc-exp-year"
+						@input="
+							(event) => (form.expiryYear = event.target.value.slice(0, 2))
+						"
+					/>
+				</div>
+
+				<p
+					v-if="form.errors.expiryMonth || form.errors.expiryYear"
+					class="mt-2 text-sm text-skin-danger"
+				>
+					The expiry date is invalid
+				</p>
+			</div>
+
+			<div class="flex flex-row items-center space-x-2 sm:w-3/5">
+				<FloatingLabel
+					v-model:value="form.cvv"
+					class="w-16"
+					input-id="cvv"
+					label-text="CVV"
+					input-type="text"
+					input-autocomplete="cc-csc"
+					input-mode="numeric"
+					:input-max-length="3"
+					:error-message="form.errors.cvv"
+					@input="(event) => (form.cvv = event.target.value.slice(0, 3))"
+				/>
+
+				<img
+					:src="'/images/register/payment/cvv.svg'"
+					class="h-8 w-16"
+				/>
+
+				<p class="text-sm text-skin-text-muted">
+					Last 3 digits on the back of the card
+				</p>
+			</div>
+		</div>
+
+		<div
+			class="flex items-center justify-between rounded-lg bg-skin-white bg-opacity-60 px-4 py-3"
+		>
+			<div>
+				<p class="font-bold">
+					{{ subscriptionPrice }}<span class="text-xs">/month</span>
+				</p>
+				<p class="text-skin-text-muted">{{ subscriptionPlanName }}</p>
+			</div>
+
+			<Link
+				class="font-bold text-skin-link"
+				href="/pricing"
+			>
+				Change
+			</Link>
+		</div>
+
+		<p class="mx-auto max-w-xl text-sm text-skin-text-muted">
+			By clicking the "Start paid membership" button below, you agree to our
+			Terms of Use and that you are over 18 and acknowledge the Privacy
+			Statement. The Wordsmith's Collection will automatically continue your
+			membership and charge the membership fee (currently
+			{{ subscriptionPrice }}/month) to your payment method until you cancel.
+			You may cancel at any time to avoid future charges.
+		</p>
+
+		<button
+			type="submit"
+			class="my-2 flex w-full items-center justify-center rounded-md bg-skin-secondary px-5 py-3 font-bold hover:bg-skin-secondary-offset"
+			:disabled="form.processing"
+		>
+			Verify account
+			<IconRightArrow
+				class="ml-4 h-4 w-4"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+			/>
+		</button>
+	</form>
+</template>
--- resources/js/Pages/Register/Payment.vue
+++ resources/js/Pages/Register/Payment.vue
@@ -1,9 +1,7 @@
 <script setup>
-import AppHead from "@/Components/Common/AppHead.vue";
-import FloatingLabel from "@/Components/Forms/FloatingLabel.vue";
 import RegisterLayout from "@/Layouts/RegisterLayout.vue";
-import { ref } from "vue";
-import IconRightArrow from "@icons/right-arrow.svg?component";
+import AppHead from "@/Components/Common/AppHead.vue";
+import FormPayment from "@/Forms/FormPayment.vue";
 
 const subscriptionPlan = {
 	name: "Basic",
@@ -20,12 +18,6 @@ const subscriptionPlan = {
 		},
 	],
 };
-
-const cvv = ref(null);
-const expiryMonth = ref(null);
-const expiryYear = ref(null);
-
-const stringPrice = `${subscriptionPlan.currency}${subscriptionPlan.price}`;
 </script>
 
 <template>
@@ -35,155 +27,13 @@ const stringPrice = `${subscriptionPlan.currency}${subscriptionPlan.price}`;
 	/>
 
 	<RegisterLayout
-		:header-title="'Secure payment info'"
+		header-title="Secure payment info"
 		:subscription-plan="subscriptionPlan"
 		:current-step="3"
 	>
-		<form
-			class="space-y-6"
-			action="#"
-		>
-			<FloatingLabel
-				:id="'cardholderName'"
-				label-text="Name on card"
-				:type="'text'"
-				:autocomplete="'cc-name'"
-			/>
-
-			<div>
-				<span
-					class="mb-2 flex flex-row space-x-2"
-					aria-label="We accept Visa, Mastercard and American Express."
-				>
-					<img
-						:src="'/images/register/payment/VISA.png'"
-						alt="Visa"
-						class="h-6"
-						:srcset="'/images/register/payment/VISA@2x.png 2x'"
-						data-uia="logoIcon-VISA"
-					/>
-					<img
-						:src="'/images/register/payment/MASTERCARD.png'"
-						alt="Mastercard"
-						class="h-6"
-						:srcset="'/images/register/payment/MASTERCARD.png 2x'"
-					/>
-					<img
-						:src="'/images/register/payment/AMEX.png'"
-						alt="American Express"
-						class="h-6"
-						:srcset="'/images/register/payment/AMEX@2x.png 2x'"
-					/>
-				</span>
-
-				<FloatingLabel
-					:id="'cardNumber'"
-					label-text="Card number"
-					:type="'number'"
-					:autocomplete="'cc-number'"
-					inputmode="numeric"
-					maxlength="24"
-				/>
-			</div>
-
-			<div
-				class="sm:flex sm:flex-row sm:items-center sm:justify-between sm:gap-6 max-sm:space-y-6"
-			>
-				<div
-					class="flex flex-col rounded-lg border border-skin-border bg-skin-primary-offset px-2.5 py-1 sm:w-2/5"
-				>
-					<label class="text-sm text-skin-text-muted">Expiration date</label>
-					<div>
-						<input
-							id="expiryMonth"
-							v-model="expiryMonth"
-							type="number"
-							inputmode="numeric"
-							max="12"
-							placeholder="MM"
-							class="w-16 rounded-lg border-none bg-skin-primary-offset py-0"
-							autocomplete="cc-exp-month"
-							@input="(event) => (expiryMonth = event.target.value.slice(0, 2))"
-						/>
-						/
-						<input
-							id="expiryYear"
-							v-model="expiryYear"
-							type="number"
-							inputmode="numeric"
-							min="24"
-							max="49"
-							placeholder="YY"
-							class="w-16 rounded-lg border-none bg-skin-primary-offset py-0"
-							autocomplete="cc-exp-year"
-							@input="(event) => (expiryYear = event.target.value.slice(0, 2))"
-						/>
-					</div>
-				</div>
-
-				<div class="flex flex-row items-center space-x-2 sm:w-3/5">
-					<FloatingLabel
-						:id="'cvv'"
-						v-model:value="cvv"
-						class="w-16"
-						label-text="CVV"
-						:type="'text'"
-						:autocomplete="'cc-csc'"
-						inputmode="numeric"
-						maxlength="3"
-						@input="cvv = cvv.slice(0, 3)"
-					/>
-
-					<img
-						:src="'/images/register/payment/cvv.svg'"
-						class="h-8 w-16"
-					/>
-
-					<p class="text-sm text-skin-text-muted">
-						Last 3 digits on the back of the card
-					</p>
-				</div>
-			</div>
-
-			<div
-				class="flex items-center justify-between rounded-lg bg-skin-white bg-opacity-60 px-4 py-3"
-			>
-				<div>
-					<p class="font-bold">
-						{{ stringPrice }}<span class="text-xs">/month</span>
-					</p>
-					<p class="text-skin-text-muted">{{ subscriptionPlan.name }}</p>
-				</div>
-
-				<Link
-					class="font-bold text-skin-link"
-					href="/pricing"
-				>
-					Change
-				</Link>
-			</div>
-
-			<p class="mx-auto max-w-xl text-sm text-skin-text-muted">
-				By clicking the "Start paid membership" button below, you agree to our
-				Terms of Use and that you are over 18 and acknowledge the Privacy
-				Statement. The Wordsmith's Collection will automatically continue your
-				membership and charge the membership fee (currently
-				{{ stringPrice }}/month) to your payment method until you cancel. You
-				may cancel at any time to avoid future charges.
-			</p>
-
-			<Link
-				href="/register/confirmation#form"
-				class="my-2 flex items-center justify-center rounded-md bg-skin-secondary py-3 font-bold hover:bg-skin-secondary-offset"
-			>
-				Start paid membership
-				<IconRightArrow
-					class="ml-4 h-4 w-4"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-				/>
-			</Link>
-		</form>
+		<FormPayment
+			:subscription-plan-name="subscriptionPlan.name"
+			:subscription-price="`${subscriptionPlan.currency}${subscriptionPlan.price}`"
+		/>
 	</RegisterLayout>
 </template>
