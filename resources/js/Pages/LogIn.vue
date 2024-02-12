fdacb61840160d696b9e4e407194cc7f9ec0eac7
HenestrosaDev <henestrosadev@gmail.com>
2024-02-12 16:06:45 +0100

    Extract the form from `LogIn` page into a separate `FormLogIn` file

--- resources/js/Pages/LogIn.vue
+++ resources/js/Pages/LogIn.vue
@@ -1,8 +1,8 @@
 <script setup>
 import AppHead from "@/Components/Common/AppHead.vue";
-import FloatingLabel from "@/Components/Forms/FloatingLabel.vue";
 import Logo from "@/Components/Common/Logo.vue";
 import Footer from "@/Components/Common/Footer.vue";
+import FormLogIn from "@/Forms/FormLogIn.vue";
 </script>
 
 <template>
@@ -23,71 +23,7 @@ import Footer from "@/Components/Common/Footer.vue";
 			<section
 				class="mx-4 mb-24 mt-10 max-w-lg rounded-lg border bg-skin-primary p-6 shadow sm:w-full sm:p-8"
 			>
-				<form
-					class="space-y-6"
-					action="#"
-				>
-					<h5
-						class="mb-8 text-center font-means-web text-3xl font-bold sm:mb-12"
-					>
-						Log in to your account
-					</h5>
-
-					<FloatingLabel
-						:id="'email'"
-						label-text="Your email or username"
-						:type="'email'"
-						:autocomplete="'email'"
-					/>
-					<FloatingLabel
-						:id="'password'"
-						label-text="Your password"
-						:type="'password'"
-						:autocomplete="'password'"
-					/>
-
-					<div class="flex items-start">
-						<div class="flex items-start">
-							<div class="flex h-5 items-center">
-								<input
-									id="remember"
-									type="checkbox"
-									value=""
-									class="focus:ring-3 h-4 w-4 cursor-pointer rounded border border-skin-border focus:ring-skin-secondary"
-									required
-								/>
-							</div>
-							<label
-								for="remember"
-								class="ms-2 text-sm font-medium"
-							>
-								Remember me
-							</label>
-						</div>
-						<a
-							href="#"
-							class="ms-auto hidden text-sm text-skin-link hover:underline"
-						>
-							Forgot password?
-						</a>
-					</div>
-					<button
-						type="submit"
-						class="w-full rounded-lg !bg-skin-secondary px-5 py-2.5 text-center text-sm font-bold hover:!bg-skin-secondary-offset focus:outline-none focus:ring-4 focus:ring-skin-border"
-					>
-						Log in to your account
-					</button>
-					<div class="text-center text-sm font-medium text-skin-text-muted">
-						Not registered?
-						<Link
-							href="/pricing"
-							class="text-skin-link hover:underline"
-						>
-							Select a subscription plan
-						</Link>
-						to create your account
-					</div>
-				</form>
+				<FormLogIn />
 			</section>
 		</div>
 
