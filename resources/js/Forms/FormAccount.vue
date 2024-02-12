881719e826aeac73c4b1b2c017c1f698bccd4acf
HenestrosaDev <henestrosadev@gmail.com>
2024-02-12 20:38:38 +0100

    Extract the form from the `Account` page into a separate `FormAccount`

---
+++ resources/js/Forms/FormAccount.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+import FloatingLabel from "@/Components/Forms/FloatingLabel.vue";
+import Checkbox from "@/Components/Forms/Checkbox.vue";
+import IconRightArrow from "@icons/right-arrow.svg?component";
+
+const form = useForm({
+	username: null,
+	email: null,
+	password: null,
+	has_accepted_terms: false,
+});
+
+const submitForm = () => {
+	form.post("/register", {
+		onSuccess: () => {
+			form.reset();
+			form.clearErrors();
+		},
+	});
+};
+</script>
+
+<template>
+	<form
+		class="space-y-6"
+		@submit.prevent="submitForm"
+	>
+		<FloatingLabel
+			v-model:value="form.username"
+			input-id="username"
+			label-text="Your username"
+			input-type="text"
+			input-autocomplete="off"
+			:error-message="form.errors.username"
+		/>
+		<FloatingLabel
+			v-model:value="form.email"
+			input-id="email"
+			label-text="Your email"
+			input-type="email"
+			input-autocomplete="off"
+			:error-message="form.errors.email"
+		/>
+		<FloatingLabel
+			v-model:value="form.password"
+			input-id="password"
+			label-text="Your password"
+			input-type="password"
+			input-autocomplete="new-password"
+			:error-message="form.errors.password"
+		/>
+
+		<Checkbox
+			v-model:value="form.has_accepted_terms"
+			input-id="accept-terms"
+		>
+			I agree to the
+			<Link
+				class="text-skin-link underline"
+				href="/terms"
+			>
+				Terms and Conditions
+			</Link>
+		</Checkbox>
+
+		<button
+			type="submit"
+			class="my-2 flex w-full items-center justify-center rounded-md bg-skin-secondary px-5 py-3 font-bold hover:bg-skin-secondary-offset"
+			:disabled="form.processing"
+		>
+			Continue
+			<IconRightArrow
+				class="ml-4 h-4 w-4"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+			/>
+		</button>
+
+		<p class="text-center text-sm text-skin-text-muted">
+			Already have an account?
+			<Link
+				href="/login"
+				class="text-skin-link hover:underline"
+			>
+				Login here
+			</Link>
+		</p>
+	</form>
+</template>
--- resources/js/Pages/Register/Account.vue
+++ resources/js/Pages/Register/Account.vue
@@ -1,8 +1,7 @@
 <script setup>
 import AppHead from "@/Components/Common/AppHead.vue";
-import FloatingLabel from "@/Components/Forms/FloatingLabel.vue";
 import RegisterLayout from "@/Layouts/RegisterLayout.vue";
-import IconRightArrow from "@icons/right-arrow.svg?component";
+import FormAccount from "@/Forms/FormAccount.vue";
 
 const subscriptionPlan = {
 	name: "Basic",
@@ -26,87 +25,10 @@ const subscriptionPlan = {
 	/>
 
 	<RegisterLayout
-		:header-title="'Account details'"
+		header-title="Account details"
 		:subscription-plan="subscriptionPlan"
 		:current-step="1"
 	>
-		<form
-			class="space-y-6"
-			action="#"
-		>
-			<FloatingLabel
-				:id="'username'"
-				label-text="Your username"
-				:type="'text'"
-				:autocomplete="'off'"
-			/>
-			<FloatingLabel
-				:id="'email'"
-				label-text="Your email"
-				:type="'email'"
-				:autocomplete="'off'"
-			/>
-			<FloatingLabel
-				:id="'password'"
-				label-text="Your password"
-				:type="'password'"
-				:autocomplete="'new-password'"
-			/>
-
-			<div class="flex items-start">
-				<div class="flex h-5 items-center">
-					<input
-						id="remember"
-						type="checkbox"
-						value=""
-						class="focus:ring-3 h-4 w-4 cursor-pointer rounded border border-skin-border focus:ring-skin-secondary"
-						required
-					/>
-				</div>
-				<label
-					for="remember"
-					class="ms-2 text-sm font-medium"
-				>
-					<span
-						id="terms-description"
-						class="text-sm text-skin-text-muted"
-					>
-						I agree to the
-						<a
-							class="underline"
-							href="#"
-							target="_blank"
-							rel="noopener"
-						>
-							Terms and Conditions
-						</a>
-					</span>
-				</label>
-			</div>
-
-			<Link
-				href="/register/verification#form"
-				class="my-2 flex items-center justify-center rounded-md bg-skin-secondary py-3 font-bold hover:bg-skin-secondary-offset"
-			>
-				Continue
-
-				<IconRightArrow
-					class="ml-4 h-4 w-4"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-				/>
-			</Link>
-
-			<p class="text-center text-sm text-skin-text-muted">
-				Already have an account?
-				<Link
-					href="/login"
-					class="text-skin-link hover:underline"
-				>
-					Login here
-				</Link>
-			</p>
-		</form>
+		<FormAccount />
 	</RegisterLayout>
 </template>
