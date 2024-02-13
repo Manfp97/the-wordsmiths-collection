232420f282596fef9e53cbb9cc5d0f1f3b7e8426
HenestrosaDev <henestrosadev@gmail.com>
2024-02-13 11:47:56 +0100

    Add `ModalReview` component

---
+++ resources/js/Components/Modals/ModalReview.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ModalContainer from "@/Components/Modals/ModalContainer.vue";
+import FormReview from "@/Forms/FormReview.vue";
+
+defineProps({
+	modalId: {
+		type: String,
+		required: false,
+		default: "modal-review",
+	},
+	modalTitle: {
+		type: String,
+		required: false,
+		default: "Write a review",
+	},
+});
+</script>
+
+<template>
+	<ModalContainer
+		:modal-id="modalId"
+		:modal-title="modalTitle"
+	>
+		<FormReview />
+	</ModalContainer>
+</template>
