2fb5b4d4e1b75f2a467c8f50a135a046dac6adc9
HenestrosaDev <henestrosadev@gmail.com>
2024-02-14 01:12:49 +0100

    Add `build` and `esbuild` configurations to Vite config to fix `Top-level await is not available in the configured target environment ("chrome87", "edge88", "es2020", "firefox78", "safari14" + 2 overrides)` error [link](https://github.com/mozilla/pdf.js/issues/17245)

--- vite.config.js
+++ vite.config.js
@@ -30,5 +30,15 @@ export default defineConfig({
 			),
 		},
 	},
-	optimizeDeps: { esbuildOptions: { target: "esnext" } },
+	build: {
+		target: "es2022",
+	},
+	esbuild: {
+		target: "es2022",
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			target: "es2022",
+		},
+	},
 });
