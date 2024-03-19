f41e1745bf95a1d90561b70b78282e2a39c9cf61
HenestrosaDev <henestrosadev@gmail.com>
2024-03-19 12:40:06 +0100

    Add `BookCard` component

---
+++ resources/js/Components/Book/BookCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+	book: {
+		type: Object,
+		required: true,
+	},
+});
+
+const cover = props.book.cover.replaceAll(">", "class='h-full w-full'>");
+</script>
+
+<template>
+	<div
+		class="h-full select-none rounded-r-xl hover:ring-4 hover:ring-skin-card active:ring-skin-secondary"
+		:title="book.title"
+	>
+		<!-- eslint-disable vue/no-v-text-v-html-on-component vue/no-v-html -->
+		<Link
+			:href="`/book/${book.slug}`"
+			class="block h-full overflow-hidden rounded-r-xl"
+			v-html="cover"
+		/>
+		<!--eslint-enable-->
+	</div>
+</template>
