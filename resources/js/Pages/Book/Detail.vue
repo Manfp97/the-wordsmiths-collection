ed6b8605fda7d08475c16ec2c573c5d5271ef14b
HenestrosaDev <henestrosadev@gmail.com>
2024-04-02 19:01:49 +0200

    Add `whitespace-pre` to preserves whitespaces contained in the `description` of the book in `Book/Detail`

--- resources/js/Pages/Book/Detail.vue
+++ resources/js/Pages/Book/Detail.vue
@@ -143,7 +143,7 @@ const swiperBreakpoints = {
 								</span>
 							</div>
 
-							<p>{{ book.description }}</p>
+							<p class="whitespace-pre">{{ book.description }}</p>
 
 							<div class="mt-3 flex items-center space-x-4">
 								<span class="text-skin-muted">Genres</span>
