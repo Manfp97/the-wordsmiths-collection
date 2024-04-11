cae2e3fc16c41a5a29952c9d50e926ef229b74ed
HenestrosaDev <henestrosadev@gmail.com>
2024-04-12 00:20:40 +0200

    Add `Datepicker` component

---
+++ resources/js/Components/Forms/Datepicker.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { ref } from "vue";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+defineProps({
+	inputId: {
+		type: String,
+		required: true,
+	},
+	labelText: {
+		type: String,
+		required: true,
+	},
+	isRequired: {
+		type: Boolean,
+		required: false,
+		default: false,
+	},
+});
+
+const date = defineModel("value"); // eslint-disable-line
+const isOpen = defineModel("open"); // eslint-disable-line
+
+// All labels: https://vue3datepicker.com/props/localization/#aria-labels
+const ariaLabels = ref({ menu: "Some custom menu label" });
+</script>
+
+<template>
+	<div class="space-y-1">
+		<label
+			:for="inputId"
+			class="font-medium"
+		>
+			<span class="text-sm">
+				{{ labelText }}
+				<span
+					v-if="isRequired"
+					class="text-skin-danger"
+				>
+					*
+				</span>
+				<span
+					v-else
+					class="text-skin-muted"
+				>
+					(optional)
+				</span>
+			</span>
+		</label>
+
+		<VueDatePicker
+			:id="inputId"
+			v-model="date"
+			v-bind="$attrs"
+			:input-class-name="`!rounded-lg shadow-sm !py-3 hover:!border-skin-border ${
+				isOpen ? 'ring-2 ring-skin-secondary border-skin-secondary' : ''
+			}`"
+			:aria-labels="ariaLabels"
+			:required="isRequired"
+		/>
+	</div>
+</template>
