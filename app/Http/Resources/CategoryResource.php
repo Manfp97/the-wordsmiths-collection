83de78eadc8ac36949c50897421b4077ac8e111a
HenestrosaDev <henestrosadev@gmail.com>
2024-04-11 09:39:47 +0200

    Add `slug` to `CategoryResource`

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -16,6 +16,7 @@ class CategoryResource extends JsonResource
 	{
 		return [
 			'id'					=> $this->id,
+			'slug'				=> $this->slug,
 			'name'				=> $this->name,
 			'description'	=> $this->description,
 			'books'				=> BookResource::collection($this->whenLoaded('books')),
