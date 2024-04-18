800135a5c94c41905f65a0db553171ee9c1a30a0
HenestrosaDev <henestrosadev@gmail.com>
2024-04-18 20:31:41 +0200

    Fix `ResetPassword` page

--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -1,12 +1,10 @@
 <script setup>
-import GuestLayout from "@/Layouts/GuestLayout.vue";
-import InputError from "@/Components/InputError.vue";
-import InputLabel from "@/Components/InputLabel.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import TextInput from "@/Components/TextInput.vue";
-import { Head, useForm } from "@inertiajs/vue3";
+import AppHead from "@/Components/Common/AppHead.vue";
+import Logo from "@/Components/Common/Logo.vue";
+import Footer from "@/Components/Common/Footer.vue";
+import FormPasswordReset from "@/Forms/FormPasswordReset.vue";
 
-const props = defineProps({
+defineProps({
 	email: {
 		type: String,
 		required: true,
@@ -16,98 +14,42 @@ const props = defineProps({
 		required: true,
 	},
 });
-
-const form = useForm({
-	token: props.token,
-	email: props.email,
-	password: "",
-	password_confirmation: "",
-});
-
-const submit = () => {
-	form.post("/reset-password", {
-		onFinish: () => form.reset("password", "password_confirmation"),
-	});
-};
 </script>
 
 <template>
-	<GuestLayout>
-		<Head title="Reset Password" />
+	<AppHead title="Reset password" />
 
-		<form @submit.prevent="submit">
-			<div>
-				<InputLabel
-					for="email"
-					value="Email"
-				/>
+	<div class="flex min-h-screen flex-col">
+		<div
+			class="flex min-h-full flex-1 flex-col items-center justify-center bg-skin-primary"
+		>
+			<Logo
+				class="mx-4 mt-16 2xl:mt-10"
+				size="md"
+			/>
 
-				<TextInput
-					id="email"
-					v-model="form.email"
-					type="email"
-					class="mt-1 block w-full"
-					required
-					autofocus
-					autocomplete="username"
-				/>
+			<section
+				class="mx-4 mb-24 mt-10 max-w-lg rounded-lg border bg-skin-white p-6 shadow sm:w-full sm:p-8"
+			>
+				<h5 class="mb-8 text-center font-means-web text-3xl font-bold sm:mb-12">
+					Reset your password
+				</h5>
 
-				<InputError
-					class="mt-2"
-					:message="form.errors.email"
-				/>
-			</div>
-
-			<div class="mt-4">
-				<InputLabel
-					for="password"
-					value="Password"
-				/>
-
-				<TextInput
-					id="password"
-					v-model="form.password"
-					type="password"
-					class="mt-1 block w-full"
-					required
-					autocomplete="new-password"
-				/>
-
-				<InputError
-					class="mt-2"
-					:message="form.errors.password"
-				/>
-			</div>
-
-			<div class="mt-4">
-				<InputLabel
-					for="password_confirmation"
-					value="Confirm Password"
-				/>
-
-				<TextInput
-					id="password_confirmation"
-					v-model="form.password_confirmation"
-					type="password"
-					class="mt-1 block w-full"
-					required
-					autocomplete="new-password"
-				/>
-
-				<InputError
-					class="mt-2"
-					:message="form.errors.password_confirmation"
-				/>
-			</div>
-
-			<div class="mt-4 flex items-center justify-end">
-				<PrimaryButton
-					:class="{ 'opacity-25': form.processing }"
-					:disabled="form.processing"
+				<div
+					v-if="status"
+					class="mb-4 text-sm font-medium text-skin-success"
 				>
-					Reset Password
-				</PrimaryButton>
-			</div>
-		</form>
-	</GuestLayout>
+					{{ status }}
+				</div>
+
+				<FormPasswordReset
+					form-id="form-password-reset"
+					:email="email"
+					:token="token"
+				/>
+			</section>
+		</div>
+
+		<Footer />
+	</div>
 </template>
