4bbea6c59cdfea5ccb349ce0530a6c02456b3c72
HenestrosaDev <henestrosadev@gmail.com>
2024-04-19 15:22:05 +0200

    Add `SectionGoToPayment`

---
+++ resources/js/Pages/Profile/Partials/SectionGoToPayment.vue
@@ -0,0 +1,24 @@
+<template>
+	<section>
+		<header>
+			<h2 class="pb-3 font-means-web text-2xl font-bold">
+				Start your membership
+			</h2>
+
+			<p class="mt-1 text-sm text-skin-muted">
+				To enjoy the services of The Wordsmith's Collection, you will need to
+				select a subscription plan and enter a payment method to begin your
+				membership.
+			</p>
+		</header>
+
+		<div class="mt-6">
+			<Link
+				href="/subscribe"
+				class="button"
+			>
+				Select subscription plan
+			</Link>
+		</div>
+	</section>
+</template>
