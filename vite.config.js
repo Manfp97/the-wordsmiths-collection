c7b24e93cb966abcceba34700cd0278960e4d3f4
HenestrosaDev <henestrosadev@gmail.com>
2024-06-10 14:10:15 +0200

    Add `resources/css/fonts.css` to `vite.config.js` to fix error on fly.io deployment (`Unable to locate file in Vite manifest: resources/css/fonts.css`)

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ import { fileURLToPath, URL } from "url";
 export default defineConfig({
 	plugins: [
 		laravel({
-			input: ["resources/css/app.css", "resources/js/app.js"],
+			input: [
+				"resources/css/app.css",
+				"resources/css/fonts.css",
+				"resources/js/app.js",
+			],
 			refresh: true,
 		}),
 		vue({
