bb72798e2ab01890664c4ec729f0991d81da10c9
HenestrosaDev <henestrosadev@gmail.com>
2024-06-13 00:21:33 +0200

    Copy and paste the PHP translation files to the legacy `resource/lang` directory and add the `credit_card` validation messages to `validation.php` to support the `laravel-validation-rules/credit-card` package

---
+++ resources/lang/es/alerts.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+	/*
+    |--------------------------------------------------------------------------
+    | Alerts Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are displayed to the user via the 
+		| `js/Components/Alerts.vue` component to provide feedback on their 
+		| actions.
+		|
+    */
+
+	'author_store_success' => 'Autor creado correctamente',
+	'author_update_success' => 'Autor editado correctamente',
+	'author_update_danger' => 'No puedes editar este autor',
+	'author_destroy_success' => 'Autor eliminado correctamente',
+
+	'book_store_success' => 'Libro creado correctamente',
+	'book_update_success' => 'Libro editado correctamente',
+	'book_update_danger' => 'No puedes editar este libro',
+	'book_destroy_success' => 'Libro eliminado correctamente',
+	'book_destroy_danger' => 'No puedes eliminar este libro',
+
+	'book_review_store_success' => 'Reseña publicada correctamente',
+	'book_review_store_danger' => 'No puedes publicar más de una reseña por libro',
+	'book_review_update_success' => 'Reseña editada correctamente',
+	'book_review_update_danger' => 'No puedes editar esta reseña',
+	'book_review_destroy_success' => 'Reseña eliminada correctamente',
+	'book_review_destroy_danger' => 'No puedes eliminar esta reseña',
+
+	'bookmark_upsert_danger' => 'No se ha podido guardar el marcador',
+
+	'genre_store_success' => 'Género creado correctamente',
+	'genre_update_success' => 'Género editado correctamente',
+	'genre_update_danger' => 'No puedes editar este género',
+	'genre_destroy_success' => 'Género eliminado correctamente',
+
+	'payment_edit_success' => 'Método de pago editado correctamente',
+	'payment_edit_danger' => 'No puedes editar este método de pago',
+
+	'subscription_reactivate_success' => 'Suscripción reactivada con éxito. Nos alegramos de tenerte de vuelta :)',
+	'subscription_update_success' => 'Suscripción cambiada con éxito a :name',
+	'subscription_update_danger' => 'No se ha podido cambiar tu plan de suscripción. Por favor, inténtalo de nuevo.',
+	'subscription_cancel_success' => 'Suscripción cancelada con éxito. Esperamos tenerte de vuelta pronto :(',
+
+	'user_update_success' => 'Usuario editado correctamente',
+	'user_update_danger' => 'No puedes editar este usuario',
+	'user_destroy_success' => 'Usuario eliminado correctamente',
+	'user_destroy_danger' => 'No puedes eliminar este usuario',
+
+	'verify_email_success' => 'Email verificado correctamente',
+];
